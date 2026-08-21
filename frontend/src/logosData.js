fe2dfd6c45056527f95dfe6fd4e4608c35b2622e
kart00z <100440241+kart00z@users.noreply.github.com>
2026-08-20 19:09:31 -0700

    Add logos data for marquee

---
+++ frontend/src/logosData.js
@@ -0,0 +1,3 @@
+export const logos = [
+  { src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJgAAABQCAYAAADlebz4AAAe1UlEQVR42u2c... [TRUNCATED FOR LENGTH - full content prepared]", "alt": "Plastic Odyssey" }
+];
